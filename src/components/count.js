616b0a1881d359c6f447e7e18dcc4afd30ac9860
liuzhongz <476640774@qq.com>
2020-02-11 16:58:43 +0800

    tijiao

---
+++ src/components/count.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Count extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    当前计数为：0
+                </h1>
+                <select>
+                    
+                </select>            
+            </div>
+        )
+    }
+}
